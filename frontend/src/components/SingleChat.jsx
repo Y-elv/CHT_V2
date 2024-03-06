781dedffb34baa4dd0bc1fe9b65f2c6d90e923e9
Elvis MUGISHA <99681768+Y-elv@users.noreply.github.com>
2024-03-06 12:08:16 +0200

    recorrecting paths

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -14,8 +14,8 @@ import {
 import SCrollableChat from "./SCrollableChat";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSender, getSenderFull } from "../config/chatLogics";
-import ProfileModal from "./miscellaneous/ProfileModal.jsx";
-import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal.jsx";
+import ProfileModal from "../components/miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "../components/miscellaneous/UpdateGroupChatModal";
 import io from "socket.io-client";
 
 
