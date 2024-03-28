1ce53e38e93e1d2fac428da89ea7608104d5bf43
Elvis MUGISHA <99681768+Y-elv@users.noreply.github.com>
2024-03-28 19:30:23 +0200

    adding all necessary

--- frontend/src/components/newsCard/newsCard.jsx
+++ frontend/src/components/newsCard/newsCard.jsx
@@ -1,17 +1,27 @@
 import dating from "../../assets/dating.png"
 import "./newsCard.css"
 const NewsCard = ({img,text}) => {
-    return (  
-        <>
-    <div className="whole-card">
-        <div className="texts">
-       {text}
+    const isSmallDevice = window.innerWidth <= 768;
+    return (
+      <>
+        <div className="whole-card-mobile">
+          {isSmallDevice ? (
+            <div className="mobile-arr">
+              <div className="news-pics-mobile">
+                <img src={img} />
+              </div>
+              <div className="mobile-texts">{text}</div>
+            </div>
+          ) : (
+            <>
+              <div className="texts">{text}</div>
+              <div className="news-pics">
+                <img src={img} />
+              </div>
+            </>
+          )}
         </div>
-        <div className="news-pics">
-            <img src={img}/>
-        </div>
-    </div>
-        </>
+      </>
     );
 }
  
