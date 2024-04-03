d45aa482388936e75a0b1e9c42b08d8dd3bee87a
Elvis MUGISHA <99681768+Y-elv@users.noreply.github.com>
2024-04-03 06:35:51 +0200

    adding a link to the game for web

--- frontend/src/pages/game/game.jsx
+++ frontend/src/pages/game/game.jsx
@@ -2,26 +2,30 @@ import gamePic from "../../assets/gamePic.jpg"
 import { CiPlay1 } from "react-icons/ci";
 import "./game.css"
 const Game = () => {
-    return (  
-        <>
+     const gameUrl = "https://kundwahealth.org/funhealth/funhealth.html";
+    return (
+      <>
         <div className="game-container">
-            <div className="word">
-                <h5>PLAY NOW</h5>
-                <h5>shutters and ladder game</h5>
-                <h5>Available soon !</h5>
-
-            </div>
-            <div className="game-img">
-                <img src={gamePic}/>
-            </div>
-            <div className="game-icon">
-            <CiPlay1 style={{ 
-                fontSize: '2em',
-                color:'black'
-         }}/>
-            </div>
+          <div className="word">
+            <h5>PLAY NOW</h5>
+            <h5>shutters and ladder game</h5>
+            <h5>Available soon !</h5>
+          </div>
+          <div className="game-img">
+            <img src={gamePic} />
+          </div>
+          <div className="game-icon">
+            <a href={gameUrl} target="_blank" rel="noopener noreferrer">
+              <CiPlay1
+                style={{
+                  fontSize: "2em",
+                  color: "black",
+                }}
+              />
+            </a>
+          </div>
         </div>
-        </>
+      </>
     );
 }
  
